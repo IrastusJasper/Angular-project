498bf9f845b4c8d379091ecabc9c18a177ff0f66
IrastusJasper <jasperirastus@gmail.com>
2022-01-17 21:46:36 +0530

    Passing Data with Event and Property

--- recipeapp/src/app/app.module.ts
+++ recipeapp/src/app/app.module.ts
@@ -4,10 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
@@ -15,10 +15,10 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     AppComponent,
     HeaderComponent,
     RecipesComponent,
-    RecipeListComponent,
     RecipeDetailComponent,
-    RecipeItemComponent,
+    RecipeListComponent,
     ShoppingListComponent,
+    RecipeItemComponent,
     ShoppingEditComponent
   ],
   imports: [
--- recipeapp/src/app/shopping-list/shopping-list.component.ts
+++ recipeapp/src/app/shopping-list/shopping-list.component.ts
@@ -4,15 +4,16 @@ import { Ingredient } from '../Shared/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css'],
+  styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [
-    new Ingredient('Apple', 5),
-    new Ingredient('Tomatoes', 10),
-  ];
+ingredients : Ingredient []=[
+  new Ingredient('Apple', 5),
+  new Ingredient('Tomatoes', 10)
+]
+  constructor() { }
 
-  constructor() {}
+  ngOnInit(): void {
+  }
 
-  ngOnInit(): void {}
 }
