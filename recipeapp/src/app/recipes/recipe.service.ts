d6d22881dc449a284975fe08445bbad291c58308
IrastusJasper <jasperirastus@gmail.com>
2022-02-25 22:55:51 +0530

    Adding Ingredients to Recipes

--- recipeapp/src/app/recipes/recipe.service.ts
+++ recipeapp/src/app/recipes/recipe.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../Shared/ingredient.model';
 @Injectable({
   providedIn: 'root'
 })
@@ -11,9 +12,13 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A test recipe',
-      'This sample test',
-      'https://scontent.fixm3-1.fna.fbcdn.net/v/t1.6435-9/48389878_332151957387099_1453138404337254400_n.png?_nc_cat=103&ccb=1-5&_nc_sid=924451&_nc_ohc=7WmRYsYXU0EAX--DHLE&_nc_ht=scontent.fixm3-1.fna&oh=00_AT_wbEfZRNPQ3eg4fY-wsPeXVinh21W4BKQ8y-a4nuD7mA&oe=622B9610'
+      'Big Burger',
+      'What else need to say ?',
+      'https://upload.wikimedia.org/wikipedia/commons/4/47/Hamburger_%28black_bg%29.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]
     ),
   ];
   getRecipes(){
